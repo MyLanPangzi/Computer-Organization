088ad8be26a79dddbfc470753f0f4dc3fa0373c9
1251723871@qq.com <smailXie@163.com>
2021-01-01 12:23:19 +0800

    第二题 添加注释

--- src/02/find.asm
+++ src/02/find.asm
@@ -41,31 +41,31 @@ read_str(string, 33)
 start:
 
 print_str("enter a char( enter ? exit ): ")
-read_char($s0)
+read_char($s0) # 读入字节存入s0寄存器
 print_str("\n")
 
 #判断是否为?
 li $t0, 0x3f
 beq $t0, $s0, exit
 
-la $t0, string
-li $s1, 1
+la $t0, string # 字符串起始地址
+li $s1, 1 # 循环变量
 
 for:
-    lbu $t1, 0($t0)
-    bne $s0, $t1, endfor
+    lbu $t1, 0($t0) # 取单个字节
+    bne $s0, $t1, endfor # 判断是否相等，不等则继续循环
 
     print_str("Success! Location: ")
-    print_int($s1)
+    print_int($s1) #打印找到的位置
     print_str("\n")
 
     j start
 endfor:
-    addi $t0, $t0, 1
-    addi $s1, $s1, 1
-    ble $s1, 32, for
+    addi $t0, $t0, 1 #字符串地址+1
+    addi $s1, $s1, 1 #循环变量+1
+    ble $s1, 32, for # 继续循环
 
-print_str("fail\n")
+print_str("fail\n") # 未找到 打印fail
 j start
 
 exit:
